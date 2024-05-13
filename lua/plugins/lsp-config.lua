42ff5f5cf676f088e92288ee7e16fcedd867f008
ldbaumga <ldbaumgartner01@gmail.com>
2024-05-13 00:06:25 -0500

    Added Clangd lsp and Clang-format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,35 +3,39 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
                 -- for list of supporeted lsps, go to mason-lspconfig github
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls", "pyright", "clangd" },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
 
             lspconfig.pyright.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
+            })
+
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
             })
 
             -- :h vim.lsp.buf for functions to call
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
