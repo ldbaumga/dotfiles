cb2fdb74584acb44df4de47885f3cff2054b8eb2
ldbaumga <ldbaumgartner01@gmail.com>
2024-09-21 00:53:45 -0400

    added tmux and bashrc

--- lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'christoomey/vim-tmux-navigator',
-
-    vim.keymap.set('n', '<C><leader>h', ':TmuxNavigateLeft<CR>'),
-    vim.keymap.set('n', '<C><leader>j', ':TmuxNavigateDown<CR>'),
-    vim.keymap.set('n', '<C><leader>k', ':TmuxNavigateUp<CR>'),
-    vim.keymap.set('n', '<C><leader>l', ':TmuxNavigateRight<CR>'),
-}
